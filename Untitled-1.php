fb07bae84a22439d62afdc52415755593261e9b9
faishal170710 <faishalherdiansyah170710@gmail.com>
2025-08-06 19:43:26 +0700

    Add files via upload
    Signed-off-by: faishal170710 <faishalherdiansyah170710@gmail.com>

---
+++ Untitled-1.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "faishalherdiansyah170710@gmail.com"; // Ganti dengan email kamu
+    $subject = "New Contact Message from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
